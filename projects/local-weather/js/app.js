7fb327063b48613ab3507dbecd7164066f2bc8b2
Ertan Kara <kara.ertan9@gmail.com>
2018-05-21 04:07:49 +0300

    Add information on the page

--- projects/local-weather/js/app.js
+++ projects/local-weather/js/app.js
@@ -19,6 +19,7 @@ const main = function() {
   const makeRequest = function _makeRequest(response) {
     const editedURL =
       `https://fcc-weather-api.glitch.me/api/current?lat=${response.coords.latitude}&lon=${response.coords.longitude}`;
+
     $.ajax({
       type: "GET",
       url: editedURL,
@@ -33,15 +34,61 @@ const main = function() {
 
   const handleData = function _handleData(response) {
     console.log(response);
+
+    let windDirection;
+
+    if (response.wind.deg < 90) {
+      windDirection = 'North East';
+    }
+    else if (response.wind.deg < 180) {
+      windDirection = 'South East';
+    }
+    else if (response.wind.deg < 270) {
+      windDirection = 'South West';
+    }
+    else {
+      windDirection = 'North West';
+    }
+
+
+    $('.country')
+      .text(response.sys.country);
+
+    $('.city')
+      .text(response.name);
+
+    $('.weather-icon')
+      .attr('src', response.weather[0].icon);
+
+    $('.weather-details')
+      .append(
+        '<li class="list-item">' +
+          response.weather[0].description +'</li>',
+
+        '<li class="list-item">Temperature: ' +
+          response.main.temp + '</li>',
+
+        '<li class="list-item">Pressure: ' +
+          response.main.pressure + 'hPA</li>',
+
+        '<li class="list-item">Wind: '  +
+          windDirection + ' / ' +
+          response.wind.speed + 'kph</li>'
+      );
   };
 
 
-  currentUserPosition()
-  .then(makeRequest)
-  .catch(e => {
-    if (e === 'Failed to retrieve location') alert(e);
-    else console.log(e);
-  });
+  const getWeatherInfo = function _getWeatherInfo() {
+    currentUserPosition()
+    .then(makeRequest)
+    .catch(e => {
+      if (e === 'Failed to retrieve location') alert(e);
+      else console.log(e);
+    });
+  };
+
+  // Get data on start
+  getWeatherInfo();
 };
 
 $(document).ready(main);
