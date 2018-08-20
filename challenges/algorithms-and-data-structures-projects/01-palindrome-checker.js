add5f432b6aeffd144d68e792132b1c71a19a232
Ertan Kara <kara.ertan9@gmail.com>
2018-08-20 17:34:14 +0300

    Update palindrome challenge with a shorter solution

--- challenges/algorithms-and-data-structures-projects/01-palindrome-checker.js
+++ challenges/algorithms-and-data-structures-projects/01-palindrome-checker.js
@@ -2,14 +2,20 @@
  * Palindrome is when a word is equal to itself when reversed too
  */
 
+String.prototype.reverse = function() {
+  return String(this.split('').reverse().join(''));
+}
+
 function palindrome(str) {
-  str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/gi, '').toLowerCase();
-  const reversed = str.split('').reverse().join('');
-  console.log(str);
-  if (str === reversed) {
+  const rawString = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  console.log(rawString, rawString.reverse());
+
+  if (rawString === rawString.reverse()) {
     return true;
   }
+
   return false;
 }
 
-console.log(palindrome("eye!!"));
+
+console.log(palindrome("1 eye for of 1 eye."));
