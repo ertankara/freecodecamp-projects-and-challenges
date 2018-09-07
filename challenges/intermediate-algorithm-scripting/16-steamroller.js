313ec8f42e7e2d3d910c4773c21ebfaa054257df
Ertan Kara <kara.ertan9@gmail.com>
2018-09-07 12:21:51 +0300

    Add solution of steamroller challenge

--- challenges/intermediate-algorithm-scripting/16-steamroller.js
+++ challenges/intermediate-algorithm-scripting/16-steamroller.js
@@ -4,9 +4,24 @@
  */
 
 function steamrollArray(arr) {
+  const result = [];
 
+  function flatten(array) {
+    for (const el of array) {
+      if (Array.isArray(el)) {
+        flatten(el);
+      }
+      else {
+        result.push(el);
+      }
+    }
+    return result;
+  }
+
+  return flatten(arr);
 }
 
 
 console.log(steamrollArray([[1],[2]]));
 console.log(steamrollArray([1, [2], [3, [[4]]]]));
+console.log(steamrollArray([1, {}, [3, [[4]]]]));
